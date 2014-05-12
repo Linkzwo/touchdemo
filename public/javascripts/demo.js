be4633b3a128517bf04abf0c3305379ebefcf560
Linkzwo <yannickahlers@gmail.com>
2014-05-12 15:33:21 +0200

    first touch test in lab

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -17,9 +17,20 @@ touchdemo.canvas.Scene.new({
         el.on("drag", function(e, mouse) {
             this.x = e.gesture.deltaX;
             this.y = e.gesture.deltaY;
+            
+        });
+
+        this.text = this.createElement();
+        this.text.font = '14pt Arial';
+        this.text.fillStyle = 'black';
+        this.text.fillText('touches count: 0', 25, 25);
+
+        stage.on("touch", function(e, mouse) {
+            console.log(mouse);
             console.log(e);
         });
 
         stage.append(el);
+        stage.append(this.text);
     }
 });
