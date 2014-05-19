872799a890ed5c91a1082b9d385a170db4ea427e
Yannick Ahlers <yannickahlers@gmail.com>
2014-05-19 12:39:26 +0200

    update to current state

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -10,27 +10,37 @@ touchdemo.canvas.Scene.new({
     name: "MyScene",
     ready: function(stage) {
 
-        var el = this.createElement();
-        el.fillStyle = "red";
-        el.fillRect(0, 0, 100, 100);
+        this.title = this.createElement();
+        this.title.font = '24pt Helvetica';
+        this.title.fillStyle = 'black';
+        this.title.fillText('touchdemo', 50, 50);
 
-        el.on("drag", function(e, mouse) {
-            this.x = e.gesture.deltaX;
-            this.y = e.gesture.deltaY;
-            
-        });
-
-        this.text = this.createElement();
-        this.text.font = '14pt Arial';
-        this.text.fillStyle = 'black';
-        this.text.fillText('touches count: 0', 25, 25);
+        function addEntities(x, y) {
+            var entity = Class.New("Entity", [stage]);
+            entity.rect(100); // square
+            entity.position(x, y);
+            entity.el.fillStyle = "red";
+            entity.el.fillRect(0, 0, 100, 100);
+            stage.append(entity.el);
+            return entity;
+         }
 
         stage.on("touch", function(e, mouse) {
-            console.log(mouse);
-            console.log(e);
+            //console.log(mouse);
+            //console.log(e);
         });
 
-        stage.append(el);
-        stage.append(this.text);
+        stage.on("hold", function(e, mouse){
+            var ent = addEntities(mouse.x - 50 , mouse.y - 50)
+            ent.el.on("drag", function(e, mouse) {
+                ent.position(mouse.x - 50, mouse.y - 50);
+                console.log(e);
+            });
+        });
+
+        stage.append(this.title);
+    },
+    render: function(stage){
+        stage.refresh();
     }
 });
