af81909ead4aa787c01de380dd64d8d312757090
Yannick Ahlers <yannickahlers@gmail.com>
2014-06-15 20:53:55 +0200

    added scalable objects, touch count and first menu with animation

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -2,6 +2,9 @@
 var touchdemo = touchdemo || {};
 
 touchdemo.canvas = CE.defines("canvas_id").
+    extend(Animation).
+    extend(Window).
+    extend(Spritesheet).
     ready(function() {
         touchdemo.canvas.Scene.call("MyScene");
     });
@@ -10,37 +13,143 @@ touchdemo.canvas.Scene.new({
     name: "MyScene",
     ready: function(stage) {
 
+        //Canvas variables
+        var htmlCanvas = document.getElementById('canvas_id');
+        var width = htmlCanvas.width;
+        var height = htmlCanvas.height;
+
+        //Custom variables
+        touchdemo.touchCount = 0;
+        touchdemo.isScalable = false;
+        touchdemo.selectedEntity = null;
+        touchdemo.scale = 1;
+        touchdemo.tempHeight = 0;
+        touchdemo.tempWidth = 0;
+        touchdemo.rotation = 0;
+
+        //Create title
         this.title = this.createElement();
         this.title.font = '24pt Helvetica';
         this.title.fillStyle = 'black';
         this.title.fillText('touchdemo', 50, 50);
+        stage.append(this.title);
 
+        //Create Menu + Animation
+        /*var _window, content, 
+            text = this.createElement();
+
+        _window = touchdemo.canvas.Window.new(this, 400, 150);
+         content = _window.getContent();
+
+        text.fillStyle = "black";
+        text.font = "15px Arial";
+        text.fillText("Window Test", 0, 0);
+        text.y = 10;
+        
+        content.append(text)
+        
+        _window.open(stage);*/
+
+        touchdemo.menu = Class.New("Entity", [stage]);
+        touchdemo.roundRect(-110, 100, 100, height - 200, 20, touchdemo.menu);
+        stage.append(touchdemo.menu.el);
+
+        var timelineIn = touchdemo.canvas.Timeline.new(touchdemo.menu.el);
+        touchdemo.menuIn = timelineIn.to({x: 120}, 60 * 2,  Ease.easeOutElastic);
+        var timelineOut = touchdemo.canvas.Timeline.new(touchdemo.menu.el);
+        touchdemo.menuOut = timelineOut.to({x: -110}, 60 * 3, Ease.easeOutSine);
+
+        //Touch counter
+        touchdemo.touches = Class.New("Entity", [stage]);
+        touchdemo.touches.el.font = "18pt Helvetica";
+        touchdemo.touches.el.fillStyle = 'black';
+        touchdemo.touches.el.textAlign ="end";
+        touchdemo.touches.el.fillText('touch count: 0', 0, 0);
+        touchdemo.touches.position(width -10, height -10);
+        stage.append(touchdemo.touches.el);
+
+        //Function to create entities
         function addEntities(x, y) {
             var entity = Class.New("Entity", [stage]);
             entity.rect(100); // square
             entity.position(x, y);
-            entity.el.fillStyle = "red";
+            entity.el.fillStyle = "rgba(0,0,255,0.5)";
             entity.el.fillRect(0, 0, 100, 100);
+            //entity.el.setOriginPoint("middle");
+            entity.width = 100;
+            entity.height = 100;
             stage.append(entity.el);
             return entity;
          }
 
-        stage.on("touch", function(e, mouse) {
-            //console.log(mouse);
-            //console.log(e);
+        //Stage events
+        stage.on("tap touch release", function(e, mouse) {
+            touchdemo.touchCount = e.gesture.touches.length;
+        });
+
+        stage.on("swiperight", function(e, mouse) {
+              touchdemo.menuIn.call();
+        });
+
+        stage.on("swipeleft", function(e, mouse) {
+              touchdemo.menuOut.call();
         });
 
         stage.on("hold", function(e, mouse){
-            var ent = addEntities(mouse.x - 50 , mouse.y - 50)
-            ent.el.on("drag", function(e, mouse) {
-                ent.position(mouse.x - 50, mouse.y - 50);
-                console.log(e);
-            });
+            if(e.gesture.touches.length == 1){
+                var ent = addEntities(mouse.x - 50 , mouse.y - 50)
+                ent.el.on("drag", function(e, mouse) {
+                    ent.position(mouse.x - (ent.width / 2), mouse.y - (ent.height / 2));
+                    //ent.position(mouse.x - ent.width, mouse.y - ent.height);
+                });
+                ent.el.on("transformstart", function(e, mouse){
+                    touchdemo.isScalable = true;
+                    touchdemo.selectedEntity = ent;
+                    touchdemo.tempWidth = ent.width;
+                    touchdemo.tempHeight = ent.height;
+                });
+                ent.el.on("pinch rotate transform", function(e, mouse){
+                    touchdemo.scale = e.gesture.scale;
+                    touchdemo.rotation = e.gesture.rotation;
+                });
+                ent.el.on("transformed", function(e, mouse){
+                    touchdemo.isScalable = false;
+                    touchdemo.selectedEntity = null;
+                    touchdemo.scale = 1;
+                    toucdemo.rotation = 0;
+                    touchdemo.tempWidth = 0;
+                    touchdemo.tempHeight = 0;
+                });
+            }
         });
-
-        stage.append(this.title);
     },
     render: function(stage){
+        touchdemo.touches.el.fillText('touch count: ' + touchdemo.touchCount, 0, 0);
+
+        if(touchdemo.isScalable && touchdemo.selectedEntity){
+            touchdemo.selectedEntity.height = touchdemo.tempHeight * touchdemo.scale;
+            touchdemo.selectedEntity.width = touchdemo.tempWidth * touchdemo.scale;
+            //touchdemo.selectedEntity.el.rotation = touchdemo.rotation;
+            touchdemo.selectedEntity.el.fillRect(0, 0, touchdemo.selectedEntity.width , touchdemo.selectedEntity.height);
+        }
         stage.refresh();
     }
 });
+
+touchdemo.roundRect = function(x, y, w, h, radius, entity){
+    var r = x + w;
+    var b = y + h;
+    entity.el.beginPath();
+    entity.el.strokeStyle="grey";
+    entity.el.lineWidth="2";
+    entity.el.moveTo(x+radius, y);
+    entity.el.lineTo(r-radius, y);
+    entity.el.quadraticCurveTo(r, y, r, y+radius);
+    entity.el.lineTo(r, y+h-radius);
+    entity.el.quadraticCurveTo(r, b, r-radius, b);
+    entity.el.lineTo(x+radius, b);
+    entity.el.quadraticCurveTo(x, b, x, b-radius);
+    entity.el.lineTo(x, y+radius);
+    entity.el.quadraticCurveTo(x, y, x+radius, y);
+    entity.el.stroke();
+}
